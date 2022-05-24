23637ba2db43fe5734bb2cd7e610b51372ada471
Michel Antunes Simões <msimoes@server01.foursys.local>
2022-05-24 14:38:00 -0300

    create controller methods

---
+++ src/br/com/foursys/fourcamp/fourstore/communication/MainMenu.java
@@ -0,0 +1,8 @@
+package br.com.foursys.fourcamp.fourstore.communication;
+
+import java.util.Scanner;
+
+
+public class MainMenu {
+	
+}
---
+++ src/br/com/foursys/fourcamp/fourstore/controller/ProductController.java
@@ -0,0 +1,11 @@
+package br.com.foursys.fourcamp.fourstore.controller;
+
+import br.com.foursys.fourcamp.fourstore.model.Product;
+import br.com.foursys.fourcamp.fourstore.service.ProductService;
+
+public class ProductController {
+
+	
+	
+	
+}
