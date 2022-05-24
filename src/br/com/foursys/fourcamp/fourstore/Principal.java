b132cad11c91699e1ce744233cf75d6e374b44c1
Michel Antunes Simões <msimoes@server01.foursys.local>
2022-05-24 14:42:58 -0300

    removi as linhas que o michel mandou

--- src/br/com/foursys/fourcamp/fourstore/Principal.java
+++ src/br/com/foursys/fourcamp/fourstore/Principal.java
@@ -1,14 +1,6 @@
 package br.com.foursys.fourcamp.fourstore;
 
-import java.lang.invoke.MethodHandles.Lookup;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
-
-import br.com.foursys.fourcamp.fourstore.communication.MainMenu;
-
 public class Principal {
-	
 
 	public static void main(String[] args) {
 
--- src/br/com/foursys/fourcamp/fourstore/controller/ProductController.java
+++ src/br/com/foursys/fourcamp/fourstore/controller/ProductController.java
@@ -1,11 +1,6 @@
 package br.com.foursys.fourcamp.fourstore.controller;
 
-import br.com.foursys.fourcamp.fourstore.model.Product;
-import br.com.foursys.fourcamp.fourstore.service.ProductService;
-
 public class ProductController {
 
-	
-	
 	
 }
